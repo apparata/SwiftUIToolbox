55c78fa1192d6af4dc972c3bc418f6768bdac468
Martin Johannesson <mjohannesson@gmail.com>
2019-11-14 15:45:54 +0100

    Add UIViewPreview, UIViewControllerPreview, ActionButtonStyle

---
+++ Sources/SwiftUIToolbox/Button Styles/ActionButtonStyle.swift
@@ -0,0 +1,30 @@
+//
+//  Copyright © 2019 Apparata AB. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActionButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+      configuration.label
+        .padding(EdgeInsets(top: 2, leading: 10, bottom: 4, trailing: 10))
+        .foregroundColor(configuration.isPressed ? Color.white : Color.accentColor)
+        .background(configuration.isPressed ? Color.blue : Color(.sRGB, red: 0.95, green: 0.95, blue: 0.97, opacity: 1))
+        .cornerRadius(.infinity)
+    }
+}
+
+struct BorderedButton_Preview: PreviewProvider {
+  static var previews: some View {
+    Group {
+        Button(action: { }) {
+            Text("Open")
+                
+        }.buttonStyle(ActionButtonStyle())
+    }
+    .previewLayout(.sizeThatFits)
+    .padding(10)
+    .background(Color.white)
+  }
+}
---
+++ Sources/SwiftUIToolbox/UIView Preview/UIViewControllerPreview.swift
@@ -0,0 +1,26 @@
+//
+//  Copyright © 2019 Apparata AB. All rights reserved.
+//
+
+#if canImport(SwiftUI) && canImport(UIKit) && DEBUG
+import SwiftUI
+import UIKit
+
+@available(iOS 13.0, *)
+struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+    let viewController: ViewController
+
+    init(_ builder: @escaping () -> ViewController) {
+        viewController = builder()
+    }
+
+    // MARK: - UIViewControllerRepresentable
+    func makeUIViewController(context: Context) -> ViewController {
+        viewController
+    }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: UIViewControllerRepresentableContext<UIViewControllerPreview<ViewController>>) {
+        return
+    }
+}
+#endif
