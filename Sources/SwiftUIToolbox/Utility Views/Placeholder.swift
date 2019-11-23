d2029ff86e7253969cc6964ecc3543137adc68bf
Martin Johannesson <mjohannesson@gmail.com>
2019-11-23 15:24:56 +0100

    Add Placeholder view.

---
+++ Sources/SwiftUIToolbox/Utility Views/Placeholder.swift
@@ -0,0 +1,39 @@
+//
+//  Copyright © 2019 Apparata AB. All rights reserved.
+//
+
+import SwiftUI
+
+public struct Placeholder: View {
+    
+    public let cornerRadius: CGFloat
+    public let foreground: Color
+    public let background: Color
+    
+    public var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                RoundedRectangle(cornerRadius: 6)
+                    .foregroundColor(self.background)
+                Text("\(Self.numberFormatter.string(from: geometry.size.width as NSNumber) ?? "?") x \(Self.numberFormatter.string(from: geometry.size.height as NSNumber) ?? "?")")
+                    .foregroundColor(self.foreground)
+            }
+        }
+    }
+    
+    public init(cornerRadius: CGFloat = 0, foreground: Color? = nil, background: Color? = nil) {
+        self.cornerRadius = cornerRadius
+        self.foreground = foreground ?? Color(.sRGB, white: 0.0, opacity: 0.2)
+        self.background = background ?? Color(.sRGB, white: 0.0, opacity: 0.05)
+    }
+    
+    private static let numberFormatter: NumberFormatter = {
+        var formatter = NumberFormatter()
+        formatter.usesSignificantDigits = false
+        formatter.minimumIntegerDigits = 1
+        formatter.maximumIntegerDigits = 42
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+}
