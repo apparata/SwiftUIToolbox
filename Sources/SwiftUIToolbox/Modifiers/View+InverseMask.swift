b560cfe9091733d3a6321fb31f424a205bc4d20a
Martin Johannesson <mjohannesson@gmail.com>
2020-04-14 00:15:14 +0200

    Add public to inverseMask modifier.

--- Sources/SwiftUIToolbox/Modifiers/View+InverseMask.swift
+++ Sources/SwiftUIToolbox/Modifiers/View+InverseMask.swift
@@ -4,12 +4,13 @@
 
 import SwiftUI
 
-extension View {
+public extension View {
+    
     func inverseMask<Mask>(_ mask: Mask) -> some View where Mask: View {
         self.mask(mask
             .foregroundColor(.black)
             .background(Color.white)
             .compositingGroup()
             .luminanceToAlpha())
-  }
+    }
 }
