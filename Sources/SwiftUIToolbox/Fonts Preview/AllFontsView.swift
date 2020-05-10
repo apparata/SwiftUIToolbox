f03e2c20afa6b3b709dab683c6ee715e798d8edf
Martin Johannesson <mjohannesson@gmail.com>
2020-05-10 19:54:11 +0200

    Revert "Add macOS support to AllFontsView"
    This reverts commit c138e6a1b96c4540be65a7540b4044378a7e222d.

--- Sources/SwiftUIToolbox/Fonts Preview/AllFontsView.swift
+++ Sources/SwiftUIToolbox/Fonts Preview/AllFontsView.swift
@@ -2,26 +2,20 @@
 //  Copyright © 2020 Apparata AB. All rights reserved.
 //
 
-#if canImport(SwiftUI) && DEBUG
+#if canImport(SwiftUI) && canImport(UIKit) && DEBUG
 import SwiftUI
-#if canImport(UIKit)
 import UIKit
-typealias KitFont = UIFont
-#else
-import Cocoa
-typealias KitFont = NSFont
-#endif
 
 public struct AllFontsView: View {
     
     public var body: some View {
-        List(KitFont.familyNames, id: \.self) { fontFamily in
+        List(UIFont.familyNames, id: \.self) { fontFamily in
             VStack(alignment: .leading) {
                 Text(fontFamily)
                     .font(.system(size: 18, weight: .bold, design: .default))
                     .padding(4)
                     .padding(.bottom)
-                ForEach(KitFont.fontNames(forFamilyName: fontFamily), id: \.self) { font in
+                ForEach(UIFont.fontNames(forFamilyName: fontFamily), id: \.self) { font in
                     Text(font)
                         .font(.custom(font, size: 18))
                 }
