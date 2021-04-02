7b21b699a196088ae8ee56cacdd7e3342f48ed2d
Martin Johannesson <mjohannesson@gmail.com>
2021-04-03 00:29:42 +0200

    Add .smoothSpring as Animation extension.

---
+++ Sources/SwiftUIToolbox/Animation/Animation+SmoothSpring.swift
@@ -0,0 +1,10 @@
+//
+//  Copyright © 2021 Apparata AB. All rights reserved.
+//
+
+import SwiftUI
+
+extension Animation {
+    
+    public static let smoothSpring = Animation.spring(response: 0.75, dampingFraction: 0.9, blendDuration: 0)
+}
